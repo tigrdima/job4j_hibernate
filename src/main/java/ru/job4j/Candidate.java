030828446e18ce6d3f5df0b59e7955cb62ea58d5
tigrdima <duys79yandex.ru>
2022-06-03 14:43:27 +0300

    3. HQL [#6874]

---
+++ src/main/java/ru/job4j/Candidate.java
@@ -0,0 +1,86 @@
+package ru.job4j;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "candidate")
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String experience;
+    private int salary;
+
+    public Candidate() {
+    }
+
+    public Candidate(int id, String name, String experience, int salary) {
+        this.id = id;
+        this.name = name;
+        this.experience = experience;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Candidate{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", experience='" + experience + '\''
+                + ", salary=" + salary
+                + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/HbmRun.java
@@ -0,0 +1,59 @@
+package ru.job4j;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import org.hibernate.query.*;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            Candidate candidate1 = new Candidate(0, "коля", "10 лет", 20000);
+            Candidate candidate2 = new Candidate(1, "Вася", "3 года", 5000);
+            Candidate candidate3 = new Candidate(2, "Вова", "1 год", 1000);
+
+            session.save(candidate1);
+            session.save(candidate2);
+            session.save(candidate3);
+
+            Query queryAll = session.createQuery("from Candidate ");
+            for (Object st : queryAll.list()) {
+                System.out.println(st);
+            }
+
+            Query queryId = session
+                    .createQuery("from Candidate c where c.id = :Id")
+                    .setParameter("Id", 1);
+            System.out.println(queryId.uniqueResult());
+
+            Query queryName = session
+                    .createQuery("from Candidate c where c.name = :Name")
+                    .setParameter("Name", "Вова");
+            System.out.println(queryName.uniqueResult());
+
+             session.createQuery("UPDATE Candidate c set c.salary = :cNewSalary where c.name = :Name")
+                    .setParameter("cNewSalary", 130000)
+                    .setParameter("Name", "Вася")
+                    .executeUpdate();
+
+            session.createQuery("delete from Candidate where id = :Id")
+                            .setParameter("Id", 0)
+                            .executeUpdate();
+
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
